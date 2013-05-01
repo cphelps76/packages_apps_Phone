7ed997a710520dad364a27bfd1066db24d879088
cphelps76 <cphelps76@gmail.com>
2013-05-01 13:48:28 -0500

    Revert "Default back to LTE/WCDMA/GSM if supported"
    This reverts commit 1238ddbf11d5ff4cdc70bcdf09c9fffa028ab431.

--- src/com/android/phone/Use2GOnlyCheckBoxPreference.java
+++ src/com/android/phone/Use2GOnlyCheckBoxPreference.java
@@ -52,10 +52,7 @@ public class Use2GOnlyCheckBoxPreference extends CheckBoxPreference {
     protected void  onClick() {
         super.onClick();
 
-        int bestAvailableType = (mPhone.getLteOnGsmMode() == 0) ?
-                                Phone.NT_MODE_WCDMA_PREF : Phone.NT_MODE_LTE_GSM_WCDMA;
-        int networkType = isChecked() ? Phone.NT_MODE_GSM_ONLY : bestAvailableType;
-
+        int networkType = isChecked() ? Phone.NT_MODE_GSM_ONLY : Phone.NT_MODE_WCDMA_PREF;
         Log.i(LOG_TAG, "set preferred network type="+networkType);
         android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
                 android.provider.Settings.Global.PREFERRED_NETWORK_MODE, networkType);
